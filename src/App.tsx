8b5ca0a9088dd5be2475cdc4cb9a74801b90767f
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-05-02 20:16:51 -0300

    started developing and adding a form to include new products

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useEffect, useState } from 'react'
+
+interface IProduct{
+  id: number;
+  name: string;
+  price: number;
+}
+
+const url = "http://localhost:3000/products";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    async function fetchData(){
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  //2 - adição de produtos
+  const handleSubmit = async (e) => {
+
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product: IProduct) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+          </li>
+        ))}
+      </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" name="name" id="name" value={name} onChange={handleChange}/>
+          </label>
+
+          <label>
+            Preço:
+            <input type="text" name="price" id="price" value={price} onChange={handleChange}/>
+          </label>
+
+          <input type="submit" value="Criar" />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
